0429fa2a68a1c4b9f7e304f7f4358a96cbb0230d
tymcl <415533011@qq.com>
2013-11-19 10:12:14 +0800

    错误目录

--- 3.Do/naier/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//配置文件
-	include_once './config/config.inc.php';
-	//数据库连接
-	include_once './config/db.php';
-	header("Location:".$base."/admin/login.php");
-?>
--- 3.Do/naier/interface/common.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//配置文件
-	include_once '../config/config.inc.php';
-	//数据库连接
-	include_once '../config/db.php';
-?>
